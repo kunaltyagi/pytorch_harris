fb376e7dc74779256f6dcf1b19bd1c9fbda4824d
Kunal Tyagi <tyagi.kunal@live.com>
2021-06-03 19:34:50 +0900

    Shi-Tomasi seems to be done

--- model.py
+++ model.py
@@ -16,16 +16,18 @@ class Harris(nn.Module):
     1, C, H, W
     """
 
-    def __init__(self, shape: Tuple[int, int, int, int], dtype=torch.float32, use_abs=False):
+    def __init__(self, shape: Tuple[int, int, int, int], dtype=torch.float32):
         super(Harris, self).__init__()
         self.shape = shape
         self.dtype = dtype
-        scharr_x = torch.tensor([[-3, 0, 3], [-10, 0, 10], [-3, 0, 3]], dtype=self.dtype)
+        scharr_x = torch.tensor(
+            [[-3, 0, 3], [-10, 0, 10], [-3, 0, 3]], dtype=self.dtype
+        )
         scharr_y = scharr_x.transpose(0, 1)
 
         # tensor.shape = out_channels, in_channel, k, k
         self.scharr = torch.stack([scharr_x, scharr_y]).unsqueeze(1)
-        self.use_abs = use_abs
+        self.window_size = 3  # needs to be odd
 
     def forward(self, x):
         gray = tv.transforms.Grayscale()
@@ -33,13 +35,48 @@ class Harris(nn.Module):
         # maintain size with kernel size 3
         edges = F.conv2d(image, self.scharr, padding=1)
 
-        if self.use_abs:
-            abs_edges = edges.abs()
-        else:
-            abs_edges = edges.multiply(edges).sqrt_()
+        # Shi-Tomasi
+        # Ix*Ix and Iy*Iy
+        edges_sqr = edges.multiply(edges)
+        # Ix*Iy
+        # edges_xy = edges.prod(dim=1)  # Doesn't work with OpenVINO
+        edges_xy = edges[:, 0, ::] * edges[:, 1, ::]
+        # Put them together
+        edges_prod = torch.cat([edges_sqr.squeeze(0), edges_xy]).unsqueeze(0)
+        # In total 3 layers
+        conv_dims = 3
+        # sum all elements in a window
+        M = F.conv2d(
+            edges_prod,
+            padding=self.window_size // 2,
+            groups=conv_dims,
+            weight=torch.ones(conv_dims, 1, self.window_size, self.window_size),
+        )
+        # Now we have:
+        # 0: \sum_{window} IxIx
+        # 1: \sum_{window} IyIy
+        # 2: \sum_{window} IxIy
 
-        edge = abs_edges.sum(dim=1)/2
-        return edge
+        # min eigen_value = trace/2 - sqrt((trace/2)**2 - determinant)
+        # trace = m[0]+m[1]
+        # determinant = m[0]m[1] - m[2]**2
+        # => eigen_value = (m[0]+m[1])/2 - sqrt(((m[0]-m[1])/2)**2+m[2]**2)
+        IxIx = M[:, 0, ::]
+        IyIy = M[:, 1, ::]
+        IxIy = M[:, 2, ::]
+        IxIx_IyIy = IxIx - IyIy
+        trace = IxIx + IyIy
+
+        eig_val = (
+            trace - (IxIx_IyIy.multiply_(IxIx_IyIy) + 4 * IxIy.multiply_(IxIy)).sqrt()
+        )
+        # apply non-max suppression, doesn't work on OpenVINO
+        local_maxima = F.max_pool2d(eig_val, 3, stride=1, padding=1) == eig_val
+        result = local_maxima * eig_val
+
+        abs_edges = edges_sqr.sqrt_()
+        edge = abs_edges.sum(dim=1) / 2
+        return edge, eig_val
 
         thresh = torch.where(edge > 140, 255, 0)
 
@@ -54,7 +91,7 @@ def export_onnx(args):
     shape = (1, 3, 300, 300)
     # Create the Model
     dtype = torch.float32
-    model = Harris(shape=shape, dtype=dtype, use_abs=args.abs)
+    model = Harris(shape=shape, dtype=dtype)
     X = torch.ones(shape, dtype=dtype)
     output_file = args.output
     print(f"Writing to {output_file}")
@@ -77,8 +114,9 @@ def export(args):
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--abs", action="store_true", help="Use abs instead of ((x)^2)^(1/2)")
-    parser.add_argument("-o", "--output", default="out/model.onnx", help="output filename")
+    parser.add_argument(
+        "-o", "--output", default="out/model.onnx", help="output filename"
+    )
 
     args, _ = parser.parse_known_args()
     args.output = Path(args.output)
--- test.py
+++ test.py
@@ -5,6 +5,24 @@ import numpy as np
 
 import depthai as dai
 
+def find_edges(gray):
+    blurred = cv.GaussianBlur(gray, (3, 3), 0)
+
+    v = np.median(blurred)
+    sigma = 0.33
+
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    edged = cv.Canny(image, lower, upper)
+    return edged
+
+def find_features(img, gray):
+    corners = cv.goodFeaturesToTrack(gray,25,0.01,5)
+    for i in corners[0]:
+        x,y = i.ravel()
+        cv.circle(img,(x,y),3,255,-1)
+    return img
+
 if __name__ == "__main__":
     pipeline = dai.Pipeline()
     # Source
@@ -35,21 +53,26 @@ if __name__ == "__main__":
         frame = frame_buffer.get()
         img = image.get().getCvFrame()
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        blurred = cv.GaussianBlur(gray, (3, 3), 0)
-        v = np.median(blurred)
-        sigma = 0.33
-        lower = int(max(0, (1.0 - sigma) * v))
-        upper = int(min(255, (1.0 + sigma) * v))
-        # edged = cv.Canny(image, lower, upper)
         # Model output
         layer = np.array(frame.getFirstLayerFp16())
+        # layer = np.array(frame.getLayerFp16(frame.getAllLayers()[0]))
         # deal with inf
         layer[layer == np.Inf] = 255
         # Reshape
         layer = np.array(layer, dtype=np.uint8)
         shape = (300, 300, 1)
         frame_data = layer.reshape(shape)
-        cv.imshow("Image", frame_data)
-        cv.imshow("CV", img)
+
+        cv.imshow("Image", img)
+        edge = False
+        if edge:
+            # edged = find_edges(gray)
+            cv.imshow("Device Edges", frame_data)
+            # cv.imshow("Host Edges", edged)
+        else:
+            gftt = find_features(img, gray)
+            cv.imshow("GFTT", gftt)
+            cv.imshow("Host-side", frame_data)
+
         if cv.waitKey(1) == ord("q"):
             break
